4869b747892a40d99b6adc67f364b58dfe547d97
Gareth Compton <copper.compton@gmail.com>
2019-08-12 15:53:40 +0100

    Mixins

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,6 @@ var gulp = require("gulp");
 var browserSync = require("browser-sync").create();
 var sass = require("gulp-sass");
 
-// Static server
-gulp.task("browser-sync", function() {
-	browserSync.init({
-		server: {
-			baseDir: "./"
-		}
-	});
-	gulp.watch("**/*.html").on("change", browserSync.reload);
-});
-
 // Compile sass into CSS & auto-inject into browsers
 gulp.task("sass", function() {
 	return gulp
@@ -20,3 +10,20 @@ gulp.task("sass", function() {
 		.pipe(gulp.dest("css"))
 		.pipe(browserSync.stream());
 });
+
+// Static server
+gulp.task(
+	"browser-sync",
+	gulp.series("sass", function() {
+		browserSync.init({
+			server: {
+				baseDir: "./"
+			}
+		});
+
+		gulp.watch("scss/*.scss", gulp.parallel("sass"));
+		gulp.watch("**/*.html").on("change", browserSync.reload);
+	})
+);
+
+gulp.task("default", gulp.parallel("browser-sync"));
